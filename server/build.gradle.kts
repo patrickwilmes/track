537d17010b2465d8cc60a455055a333299673404
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2021-12-11 13:50:16 +0100

    Basic setup of the server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(project(":core"))
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
